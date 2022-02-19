89a1bfbd54800e9891032175c0f08064fbdd94e9
cofob <cofob@riseup.net>
2022-02-19 21:53:09 +0700

    Fix Docstrings

--- autodonate/lib/context_processors/__init__.py
+++ autodonate/lib/context_processors/__init__.py
@@ -1,6 +1,4 @@
-"""
-The file is responsible for registering new global variables in templates.
-"""
+"""The file is responsible for registering new global variables in templates."""
 
 __all__ = ["register_variable", "global_variables"]
 
@@ -8,8 +6,7 @@ VALUES: dict[str, str | bool] = {}
 
 
 def register_variable(name: str, value: str | bool) -> None:
-    """
-    Add a new global variable.
+    """Add a new global variable.
 
     Args:
         name: variable name (upper case)
@@ -19,8 +16,7 @@ def register_variable(name: str, value: str | bool) -> None:
 
 
 def global_variables(request) -> dict[str, str | bool]:
-    """
-    Return a dictionary of all global variables.
+    """Return a dictionary of all global variables.
 
     Called on every render() by Django itself.
     """
---
+++ autodonate/lib/models.py
@@ -0,0 +1,65 @@
+"""Basic objects of donations, items."""
+
+from django.db.models import (
+    Model,
+    SmallIntegerField,
+    FloatField,
+    TextField,
+    BooleanField,
+    ForeignKey,
+    CASCADE,
+    CharField,
+    TimeField,
+)
+from autodonate.lib.utils.rcon import Rcon
+
+
+class Item(Model):
+    """Model for the given item"""
+
+    currency = SmallIntegerField(
+        choices=[(0, "RUB"), (1, "UAH"), (2, "USD"), (3, "EUR")], null=True
+    )
+    price = FloatField(null=True)
+    rcon_command = TextField(null=True)
+    require_nick = BooleanField(default=False)
+
+
+class PaymentProcess(Model):
+    """Model for pending item payment"""
+
+    # TODO: Add more fields
+    item = ForeignKey(Item, on_delete=CASCADE)
+    nickname = CharField(max_length=32, null=True)
+    timestamp = TimeField(auto_now_add=True)
+
+    def cleanup_nickname(self) -> None:
+        self.nickname = (
+            self.nickname.replace(" ", "")
+            .replace("'", "")
+            .replace('"', "")
+            .replace(",", "")
+            .replace(".", "")
+            .replace("`", "")
+        )
+        self.save()
+
+
+class Payment(Model):
+    """Model for finished item payment"""
+
+    process = ForeignKey(PaymentProcess, on_delete=CASCADE)
+    timestamp = TimeField(auto_now_add=True)
+
+    def format_rcon(self) -> str:
+        self.process.cleanup_nickname()
+        if not self.process.item.rcon_command:
+            raise ValueError("Item.rcon_command required.")
+        return str(
+            self.process.item.rcon_command.format(
+                nickname=self.process.nickname
+            )
+        )
+
+    def issue(self) -> str:
+        return Rcon.run(self.format_rcon())
