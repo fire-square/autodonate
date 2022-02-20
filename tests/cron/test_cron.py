9a0d7b61f6bcc8cd0eda6fb89033f92957dae2a5
cofob <cofob@riseup.net>
2022-02-20 21:52:21 +0700

    Add global cron thread (#13)
    Fixes #12

---
+++ tests/cron/test_cron.py
@@ -0,0 +1,10 @@
+from autodonate.lib.utils.cron import register_function, callbacks, Callback
+
+
+def test_cron_job_add():
+    def func():
+        pass
+
+    c = Callback(function=func, timeout=5)
+    register_function(func, 5)
+    assert callbacks == [c]
