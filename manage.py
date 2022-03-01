36100cf95f9ba9b593e6253c1fa4b9fb183092c1
PerchunPak <perchunpak@gmail.com>
2022-03-01 23:35:45 +0200

    Change `import ?` to `from ? import ?`

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
-import argparse
+from argparse import ArgumentParser
 
 from aiohttp import web
 
 from autodonate import app
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser = ArgumentParser(description="Process some integers.")
     parser.add_argument("action", metavar="N", type=str)
 
     args = parser.parse_args()
