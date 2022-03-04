b2910710fc11af01b283e1c357c4162edc3c2dc7
cofob <cofob@riseup.net>
2022-03-04 17:45:56 +0700

    Add LOG_SEPARATE and LOG_PATH features

--- autodonate/__init__.py
+++ autodonate/__init__.py
@@ -1,4 +1,4 @@
-from autodonate.utils.logger import get_logger
 from autodonate.app import app
+from autodonate.utils.logger import get_logger
 
 __all__ = ["app", "get_logger"]
--- autodonate/utils/logger.py
+++ autodonate/utils/logger.py
@@ -1,8 +1,16 @@
 """File to automatically create a logger for each file."""
 
-from logging import CRITICAL, DEBUG, INFO, FileHandler, Formatter, StreamHandler, getLogger
-from pathlib import Path
+from logging import (
+    CRITICAL,
+    DEBUG,
+    INFO,
+    FileHandler,
+    Formatter,
+    StreamHandler,
+    getLogger,
+)
 from os import environ
+from pathlib import Path
 
 __all__ = ["get_logger"]
 
@@ -12,10 +20,11 @@ def get_logger(name: str):
 
     Taken from https://stackoverflow.com/questions/59254843/add-custom-log-records-in-django.
     """
-    path = Path("logs")
+    path = Path(environ.get("LOG_PATH", "logs"))
     path.mkdir(exist_ok=True)
     logger = getLogger(name)
     log_level = environ.get("LOG_LEVEL", "INFO").upper()
+    log_separate = bool(int(environ.get("LOG_SEPARATE", 0)))
 
     if log_level == "DEBUG":
         logger.setLevel(DEBUG)
@@ -25,11 +34,11 @@ def get_logger(name: str):
         logger.setLevel(INFO)
 
     file_handler = FileHandler(
-        filename="{0}.log".format(str(path / name)),
+        filename="{0}.log".format(str(path / (name if log_separate else "autodonate"))),
         encoding="utf8",
     )
     console_handler = StreamHandler()
-    file_formatter = Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    file_formatter = Formatter("%(asctime)s [{0}:%(levelname)s] %(message)s".format(name))
     console_formatter = Formatter("[{0}:%(levelname)s] %(message)s".format(name))
     file_handler.setFormatter(file_formatter)
     console_handler.setFormatter(console_formatter)
