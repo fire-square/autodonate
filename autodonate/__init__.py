b52038f8b2d509fc9bd66c561424802fe2215a85
cofob <cofob@riseup.net>
2022-03-04 17:47:17 +0700

    Port logger from django branch :truck: (#30)
    * Port logger from django branch :truck:
    
    * Add log levels
    
    * Add @PerchunPak suggestions
    
    * Add LOG_SEPARATE and LOG_PATH features

--- autodonate/__init__.py
+++ autodonate/__init__.py
@@ -1,1 +1,4 @@
 from autodonate.app import app
+from autodonate.utils.logger import get_logger
+
+__all__ = ["app", "get_logger"]
--- autodonate/app.py
+++ autodonate/app.py
@@ -1,4 +1,7 @@
 from aiohttp import web
+from autodonate.utils.logger import get_logger
+
+log = get_logger(__name__)
 
 
 async def handle(request):
