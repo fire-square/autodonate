4c12ea297675f9814fb4db6e66140247e37ce043
PerchunPak <perchunpak@gmail.com>
2022-03-04 22:53:14 +0200

    Port Config class from `master`

--- autodonate/__init__.py
+++ autodonate/__init__.py
@@ -1,4 +1,5 @@
 from autodonate.app import app
+from autodonate.utils.config import Config
 from autodonate.utils.logger import get_logger
 
-__all__ = ["app", "get_logger"]
+__all__ = ["app", "get_logger", "Config"]
