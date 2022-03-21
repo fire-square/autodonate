9ed863373464fd44200912cd6f4ffbf2fcca7c5c
cofob <cofob@riseup.net>
2022-03-21 17:58:51 +0700

    DL app view

--- autodonate/app.py
+++ autodonate/app.py
@@ -5,11 +5,4 @@ from autodonate.utils.logger import get_logger
 log = get_logger(__name__)
 
 
-async def handle(request):
-    name = request.match_info.get("name", "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
-
-
 app = web.Application()
-app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
