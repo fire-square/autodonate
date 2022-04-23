37bf79dd36322b94ebe7d6bfc779ac50dc33be84
PerchunPak <perchunpak@gmail.com>
2022-04-23 09:48:34 +0200

    Delete logger from models.py

--- autodonate/models.py
+++ autodonate/models.py
@@ -1,8 +1,5 @@
 """Models for our project."""
 from django.db import models
-from structlog.stdlib import get_logger
-
-log = get_logger()
 
 
 class Config(models.Model):
@@ -10,4 +7,3 @@ class Config(models.Model):
 
     key: str = models.CharField(max_length=255, unique=True, primary_key=True)
     value: str = models.CharField(max_length=255, null=True)
-    log.debug("models.Config", key=key, value=value)
