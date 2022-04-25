ce9a13736e30c76c675baaf3e61812c5d122133f
Vasiliy Petrov <cofob@riseup.net>
2022-04-25 19:07:26 +0700

    Удалил тесты, т.к. в этой ветке ещё нет `pytest_django`

--- tests/test_static.py
+++
@@ -1,9 +0,0 @@
-"""Stub."""
-
-from autodonate.lib.templatetags.statictags import static
-
-
-def test_static() -> None:
-    """Test static template tag."""
-    print(static("test/file"))
-    assert isinstance(static("test/file"), str)
