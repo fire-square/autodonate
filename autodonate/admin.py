76d85d0731b14fb7938d36531bdc6daf89f7005a
Vasiliy Petrov <cofob@riseup.net>
2022-04-25 19:39:51 +0700

    Добавил геттеры для Config

--- autodonate/admin.py
+++ autodonate/admin.py
@@ -1,22 +1,6 @@
 """Register some tables to the admin panel."""
 from django.contrib import admin
-from structlog.stdlib import get_logger
 
 from autodonate.models import Config
 
-log = get_logger()
-
-
-@admin.register(Config)
-class ConfigAdmin(admin.ModelAdmin):
-    """Admin Config model."""
-
-    def has_add_permission(self, request, obj=None):
-        """Disable add action."""
-        log.debug("ConfigAdmin.has_add_permission", request=request, obj=obj)
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        """Disable delete."""
-        log.debug("ConfigAdmin.has_delete_permission", request=request, obj=obj)
-        return False
+admin.register(Config)
--- autodonate/models.py
+++ autodonate/models.py
@@ -1,9 +1,54 @@
 """Models for our project."""
+from typing import Any
+
 from django.db import models
+from ubjson import dumpb, loadb
+
+
+class ConfigKeyDoesNotExist(Exception):
+    """Exception for Config.
+
+    Raises when row not found in DB.
+    """
+
+    pass
 
 
 class Config(models.Model):
-    """Configuration for the application."""
+    """Configuration model for the application."""
 
     key: str = models.CharField(max_length=255, unique=True, primary_key=True)
-    value: str = models.CharField(max_length=255, null=True)
+    value: str = models.BinaryField(null=True)
+
+    @classmethod
+    def get(cls, key: str) -> Any:  # type: ignore
+        """Get config row from DB.
+
+        Args:
+            key: row name
+
+        Returns:
+            Decoded ubjson string.
+
+        Raises:
+            ConfigKeyDoesNotExist: when row not found.
+        """
+        try:
+            return loadb(cls.objects.get(key=key).value)
+        except cls.DoesNotExist:
+            raise ConfigKeyDoesNotExist
+
+    @classmethod
+    def set(cls, key: str, value: Any) -> None:  # type: ignore
+        """Set (or create) config row in DB.
+
+        Args:
+            key: row name
+            value: All that understands ubjson (lists, dicts, bool, str, int, bytes...)
+        """
+        try:
+            obj = cls.objects.get(key=key)
+        except cls.DoesNotExist:
+            obj = cls.objects.create(key=key)
+        obj.value = dumpb(value)
+        obj.save()
