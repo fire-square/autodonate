fe01fcdbb2d324d66d69adaba67968887bf70a5e
Vasiliy Petrov <cofob@riseup.net>
2022-04-27 18:22:34 +0700

    Add Modal.svelte

---
+++ svelte/src/ui/Modal.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let modal;
+
+  function close() {
+    modal.classList.toggle("is-active");
+  }
+</script>
+
+<div class="modal" bind:this={modal} id="modal">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <div class="box">
+      test
+    </div>
+  </div>
+  <button class="modal-close is-large" aria-label="close" on:click={close}></button>
+</div>
