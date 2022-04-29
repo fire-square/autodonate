8fe6fa0b304aec5d9d15f94b882a08c18b45cc6c
Vasiliy Petrov <cofob@riseup.net>
2022-04-29 19:50:51 +0700

    Working modal (broken backdrop)

--- svelte/src/ui/Modal.svelte
+++ svelte/src/ui/Modal.svelte
@@ -1,35 +1,36 @@
 <script>
   export let title;
-  export let element;
   export let id;
-  import { createEventDispatcher } from 'svelte';
 
-	const dispatch = createEventDispatcher();
+  let element;
 
-  function close() {
+  function toggle() {
     element.classList.toggle("show");
-    setTimeout(function() {
-      element.remove();
-      dispatch('modalclose', {
-        id
-      });
+    setTimeout(function () {
+      element.classList.toggle("visually-hidden");
     }, 500)
+  }
+
+  function toggle_mouseout() {
+    if (element.classList.contains("show")) {
+      toggle();
+    }
   }
 </script>
 
-<div bind:this={element} class="modal fade show text-dark" tabindex="-1" style="display: block;" aria-modal="true" role="dialog"
-      id="modal-{id}">
-  <div class="modal-dialog">
+<div bind:this={element} class="modal fade visually-hidden text-dark" tabindex="-1" style="display: block;" aria-modal="true" role="dialog"
+      id="modal-{id}" data-backdrop="static" data-keyboard="false">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLiveLabel">{title}</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" on:click={toggle}></button>
       </div>
       <div class="modal-body">
         <slot></slot>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" on:click={close}>Close</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" on:click={toggle}>Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
