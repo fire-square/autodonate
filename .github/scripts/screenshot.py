bce5cc2b100c0cda306d7a4929e1695bbf4233f1
PerchunPak <perchunpak@gmail.com>
2022-05-01 11:10:14 +0200

    Upgrade script

--- .github/scripts/screenshot.py
+++ .github/scripts/screenshot.py
@@ -1,17 +1,21 @@
+"""Script for generating screenshots of the main page in ``.github/workflows/screenshot.yml``."""
+
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
-from os import listdir, mkdir, chdir, remove
-from os.path import isdir, isfile
-from shutil import rmtree
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver.get("http://localhost:8000/")
-driver.set_window_size(1920, 1080)
-sleep(5)
-driver.save_screenshot(f'screen.png')
-driver.close()
+
+def main() -> None:
+    """Main function of the script."""
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get("http://localhost:8000/")
+    driver.set_window_size(1920, 1080)
+    driver.save_screenshot("screen.png")
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
