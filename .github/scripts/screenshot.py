84a4204ad12e87e21b514464f5988473079340eb
cofob <cofob@riseup.net>
2022-05-01 17:46:57 +0700

    Add screenshot action (#116)
    Co-authored-by: PerchunPak <perchunpak@gmail.com>

---
+++ .github/scripts/screenshot.py
@@ -0,0 +1,21 @@
+"""Script for generating screenshots of the main page in ``.github/workflows/screenshot.yml``."""
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def main() -> None:
+    """Main function of the script."""
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get("http://localhost:8000/")
+    driver.set_window_size(1920, 1080)
+    driver.save_screenshot("screen.png")
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
