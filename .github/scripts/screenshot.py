4a0f88d7ea11f3787007617b62c54c4d94eee947
Vasiliy Petrov <cofob@riseup.net>
2022-05-03 13:28:27 +0000

    Update

--- .github/scripts/screenshot.py
+++ .github/scripts/screenshot.py
@@ -1,8 +1,7 @@
 """Script for generating screenshots of the main page in ``.github/workflows/screenshot.yml``."""
 
-from sys import argv
 from time import sleep
-from random import randint
+from urllib.parse import urlencode
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -15,11 +14,12 @@ def main() -> None:
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    driver.get("http://localhost:8000" + argv[1])
-    driver.set_window_size(1920, 1080)
-    sleep(5)
-    driver.save_screenshot(f"screen-{str(randint(1, 9999999))}.png")
-    driver.close()
+    for url in ["/", "/#donate", "/#about", "/django-admin", "/admin"]:
+        driver.get("http://localhost:8000" + url)
+        driver.set_window_size(1920, 1080)
+        sleep(5)
+        driver.save_screenshot(f"Screenshot+{urlencode(url)}.png")
+        driver.close()
 
 
 if __name__ == "__main__":
--- .github/scripts/upload.py
+++ .github/scripts/upload.py
@@ -1,14 +1,16 @@
-import imp
+"""Script for uploading screenshots to public storage."""
 
+from glob import glob
+from sys import argv
+from urllib.parse import unquote_plus
 
 import requests
-from sys import argv
-from glob import glob
 
-
-for image in glob("screen-*.png"):
-    r = requests.post("https://screen.deta.dev/upload",
-                    data={"secret": argv[1], "key": argv[2]},
-                    files={"file": open(image, 'rb')})
+for image in glob("Screenshot*.png"):
+    r = requests.post(
+        "https://screen.deta.dev/upload",
+        data={"secret": argv[1], "key": argv[2], "comment": unquote_plus(image)},
+        files={"file": open(image, "rb")},
+    )
 
 print("https://screen.deta.dev/" + r.json()["key"])
