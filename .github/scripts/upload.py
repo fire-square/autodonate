407cb687f757cf279c2ee1390c85be5918fbcb7f
Vasiliy Petrov <cofob@riseup.net>
2022-05-03 13:20:27 +0000

    Update

--- .github/scripts/upload.py
+++ .github/scripts/upload.py
@@ -9,6 +9,6 @@ from glob import glob
 for image in glob("screen-*.png"):
     r = requests.post("https://screen.deta.dev/upload",
                     data={"secret": argv[1], "key": argv[2]},
-                    files={"screen.png": open(image, 'rb')})
+                    files={"file": open(image, 'rb')})
 
 print("https://screen.deta.dev/" + r.json()["key"])
