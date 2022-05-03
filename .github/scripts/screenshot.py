2bf1064200a19eb2fbb293c39b3571b07bcd1dcd
cofob <cofob@riseup.net>
2022-05-03 23:36:41 +0700

    Change width

--- .github/scripts/screenshot.py
+++ .github/scripts/screenshot.py
@@ -45,9 +45,7 @@ def main() -> None:
             continue
 
         # Set window size to page size (fullpage screenshot)
-        required_width = driver.execute_script("return document.body.parentNode.scrollWidth")
-        required_height = driver.execute_script("return document.body.parentNode.scrollHeight")
-        driver.set_window_size(required_width, required_height)
+        driver.set_window_size(1920, driver.execute_script("return document.body.parentNode.scrollHeight"))
 
         # Sleep more to ensure full page load
         sleep(2)
