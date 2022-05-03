41a7d5d803db8ed4cf03ced21e21b1b5264ebd62
Vasiliy Petrov <cofob@riseup.net>
2022-05-03 13:10:56 +0000

    Update

--- .github/scripts/screenshot.py
+++ .github/scripts/screenshot.py
@@ -1,6 +1,8 @@
 """Script for generating screenshots of the main page in ``.github/workflows/screenshot.yml``."""
 
+from sys import argv
 from time import sleep
+from random import randint
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,10 +15,10 @@ def main() -> None:
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    driver.get("http://localhost:8000/")
+    driver.get("http://localhost:8000" + argv[1])
     driver.set_window_size(1920, 1080)
-    sleep(10)
-    driver.save_screenshot("screen.png")
+    sleep(5)
+    driver.save_screenshot(f"screen-{str(randint(1, 9999999))}.png")
     driver.close()
 
 
---
+++ .github/scripts/upload.py
@@ -0,0 +1,12 @@
+import imp
+
+
+import requests
+from sys import argv
+from glob import glob
+
+
+for image in glob("screen-*.png"):
+    requests.post("https://screen.deta.dev/upload",
+                    data={"secret": argv[1], "key": argv[2]},
+                    files=[open(image, 'rb')])
